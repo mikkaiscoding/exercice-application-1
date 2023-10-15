dc5c3323a3370885e63b658050026e9870531a23
mikkaiscoding <mikka.kantinti@gmail.com>
2023-10-15 16:36:07 +0200

    final commit

--- exercice_application.c
+++ exercice_application.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int main(){
+    float v, a, c;
+    printf("Saississez 1 entiers \n");
+    scanf("%f", &v);
+    printf("Vous avez saisis %2.f, saissisez un autre entier\n", v);
+    scanf("%f", &a);
+    printf("vous avez saisis %2.f et %2.f\n", v, a);
+    c = v * a;
+    printf("Le produit de %2.f et %2.f est %2.f\n", v, a, c);
+}
